71748a7266095ffb2824b1bd59bda234ae2d5f24
Craig Banach <craigsb93@gmail.com>
2016-04-07 22:22:12 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require("express");
+
+var app = express();
+
+app.get("/", function (req, res) {
+    res.end("Done");
+})
+
+app.listen(8080, function () {
+    console.log("Listening on port 8080...")
+})
